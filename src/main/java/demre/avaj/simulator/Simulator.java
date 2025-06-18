31018c7d3d2397b404e541e92c516149506e0e27
Doruk Emre <doruk.emre@outlook.com>
2025-06-18 17:30:58 +0200

    Added loading of scenario file and some validation

--- src/main/java/demre/avaj/simulator/Simulator.java
+++ src/main/java/demre/avaj/simulator/Simulator.java
@@ -1,5 +1,32 @@
 package main.java.demre.avaj.simulator;
 
-public class Simulator {
+import java.io.File;
 
+public class Simulator {
+  public static void main(String[] args) {
+    if (args.length == 1) {
+      String scenarioFileName = args[0];
+
+      if (isValidScenarioFile(scenarioFileName)) {
+        System.out.println("File '" + scenarioFileName + "' exists and is a valid file.");
+
+      } else {
+        System.out.println(
+            "\u001B[31mError:\u001B[0m file '" + scenarioFileName + "' does not exist or is not a valid file.");
+      }
+    } else {
+      System.out.println("\u001B[31mError:\u001B[0m wrong number of arguments");
+    }
+    return;
+  }
+
+  private static boolean isValidScenarioFile(String scenarioFileName) {
+    File scenarioFile = new File(scenarioFileName);
+    return (scenarioFile.exists() && scenarioFile.isFile() && isValidScenarioFileContent(scenarioFile));
+  }
+
+  private static boolean isValidScenarioFileContent(File scenarioFile) {
+
+    return (true);
+  }
 }
