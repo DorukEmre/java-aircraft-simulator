588e30861d243f4239436d6ca35e9a1518d6eebc
Doruk Emre <doruk.emre@outlook.com>
2025-06-19 22:22:44 +0200

    Refactored Baloon and JetPlane to extend Aircraft and AircraftFactory accept all Flyable types

--- src/main/java/demre/avaj/simulator/aircrafts/JetPlane.java
+++ src/main/java/demre/avaj/simulator/aircrafts/JetPlane.java
@@ -1,5 +1,13 @@
 package demre.avaj.simulator.aircrafts;
 
-public class JetPlane {
+public class JetPlane extends Aircraft {
 
+  public JetPlane(long p_id, String p_name, Coordinates p_coordinate) {
+    super(p_id, p_name, p_coordinate);
+  }
+
+  @Override
+  public void updateConditions() {
+
+  }
 }
--- src/main/java/demre/avaj/simulator/factory/AircraftFactory.java
+++ src/main/java/demre/avaj/simulator/factory/AircraftFactory.java
@@ -2,6 +2,11 @@ package demre.avaj.simulator.factory;
 
 import demre.avaj.simulator.aircrafts.*;
 
+// class AircraftFactory <<Singleton>>
+// {
+// +Flyable* newAircraft(string p_type, string p_name, Coordinates p_coordinates)
+// }
+
 public class AircraftFactory {
   // Static instance of the singleton
   private static AircraftFactory instance;
@@ -19,9 +24,18 @@ public class AircraftFactory {
   }
 
   public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates) {
-    Flyable flyable = new Flyable() {
+    Flyable flyable;
+    long id = 1; // Set unique ID
 
-    };
+    if (p_type.equals("Helicopter")) {
+      flyable = new Helicopter(id, p_name, p_coordinates);
+    } else if (p_type.equals("JetPlane")) {
+      flyable = new JetPlane(id, p_name, p_coordinates);
+    } else if (p_type.equals("Baloon")) {
+      flyable = new Baloon(id, p_name, p_coordinates);
+    } else {
+      throw new IllegalArgumentException("Unknown aircraft type: " + p_type);
+    }
 
     return flyable;
   }
